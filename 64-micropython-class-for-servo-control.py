259d39cf1031f1bf34685d5fb31d8e2e0e4006ea
frevega <elmigueluno@gmail.com>
2024-04-11 17:52:08 -0400

    Lesson 64: Improving MyServo class

--- 64-micropython-class-for-servo-control.py
+++ 64-micropython-class-for-servo-control.py
@@ -1,42 +1,16 @@
 from machine import Pin
-from MyLib import MyPWM
-from time import sleep
+from MyLib import MyServo
+from time import sleep_ms
 
-servo = MyPWM(pinNumber = 15, freq = 50)
-
-# Slope (m) equation
-#  x, y     x, y
-# (0, 8191) (180, 1638)
-# m =  (y2 - y1)
-#      ---------
-#      (x2 - x1)
-# m = (1638 - 8191) / (180 - 0)
-# m = -6553 / 180
-
-# Equation of the line
-# y - y1 = m (x - x1)
-# y - 8191 = (-6553 / 180) (x - 0)
-# y - 8191 = (-6553 / 180) x
-# y = ((-6553 / 180) x) + 8191
-# y = duty value
-# x = potentiometer value
-
-duty = None
-
-def pos_servo(angle):
-    global pot_val, duty
-    duty = int((-6553 / 180) * angle) + 8191
-    servo.duty_u16(duty)
+servo = MyServo(pin_number = 15)
 
 if __name__ == "__main__":
     try:
         while True:
-            for x in range(181):
-                pos_servo(x)
-                sleep(.005)
-            for x in range(180, -1, -1):
-                pos_servo(x)
-                sleep(.005)
+            servo.angle_from_to(180, 0)
+            sleep_ms(200)
+            servo.angle_from_to(0, 180)
+            sleep_ms(200)
     except KeyboardInterrupt:
         print("\nSee ya later, RPi Pico!")
 
--- MyLib.py
+++ MyLib.py
@@ -1,4 +1,5 @@
 from machine import Pin, PWM
+from time import sleep_ms
 
 class MyPWM(PWM):
     """
@@ -8,6 +9,48 @@ class MyPWM(PWM):
         super().__init__(Pin(pinNumber))
         self.freq(freq)
         self.duty_u16(duty_u16)
+
+class MyServo(MyPWM):
+    """
+        A class for easy Servo instantiation
+
+        Slope (m) equation
+         x, y     x, y
+        (0, 7800) (180, 1400)
+        m =  (y2 - y1)
+             ---------
+             (x2 - x1)
+        m = (1400 - 7800) / (180 - 0)
+        m = -6400 / 180
+
+        Equation of the line
+        y - y1 = m (x - x1)
+        y - 7800 = (-6400 / 180) (x - 0)
+        y - 7800 = (-6400 / 180) x
+        y = ((-6400 / 180) * x) + 7800
+        y = duty value
+        x = angle value
+    """
+    def __init__(self, pin_number: int, freq: int = 50, duty_u16: int = 0):
+        super().__init__(pinNumber = pin_number, freq = freq)
+    
+    def angle(self, angle: int):
+        duty = int((-6400 / 180) * angle) + 7800
+        self.duty_u16(duty)
+    
+    def angle_from_to(self, start_angle: int, end_angle: int, sleep_ms_time: int = 10):
+        if start_angle < end_angle:
+            start = start_angle + 1
+            stop = end_angle
+            step = 1
+        else:
+            start = start_angle
+            stop = end_angle - 1
+            step = -1
+
+        for angle in range(start, stop, step):
+            self.angle(angle)
+            sleep_ms(sleep_ms_time)
 
 class MyColor:
     """
